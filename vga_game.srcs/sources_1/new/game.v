bcef2e8e353b8fda36fb999772ef48de47deb83d
Omar Kanj <omar.kanj99@gmail.com>
2022-11-11 01:11:39 -0800

    refactored the code and added todo

--- vga_game.srcs/sources_1/new/game.v
+++ vga_game.srcs/sources_1/new/game.v
@@ -20,11 +20,22 @@ module game(
     output reg [4:0] timer_out
     );
 
+// clocking registers
 integer clock_cycles = 0;
 reg [7:0] sec_counter = 0;
 reg [9:0] ms_counter = 0; // can go between 0 -> 1000
 reg ms_clk = 0;
-// adding seconds to the counter
+
+// game registers
+reg [8:0] paddle_x_pos;
+reg [2:0] quadAr, quadBr;
+reg [4:0] ball_speed = 1;
+reg [9:0] ball_x_pos = 5;
+reg [9:0] ball_y_pos = 5;
+reg negative_x = 0;
+reg negative_y = 0;
+
+// seconds counter
 always@(posedge clk_25) begin
     // make sure the user started the game
     if (clock_cycles == 25000000) begin
@@ -36,76 +47,76 @@ always@(posedge clk_25) begin
     end
     if (clock_cycles%25000 == 0 && clock_cycles != 0) begin
         ms_clk = ~ms_clk; 
-        ms_counter = ms_counter + 1;    // Should count up to 1000 and then reset
+        ms_counter = ms_counter + 1;// Should count up to 1000 and then reset
     end 
 end
 
-always@(sec_counter) begin
-    timer_out <= sec_counter;
-end
+assign timer_out <= sec_counter;
 
-reg [8:0] PaddlePosition;
-reg [2:0] quadAr, quadBr;
+
+// paddle input
 always @(posedge clk_25) quadAr <= {quadAr[1:0], quadA};
 always @(posedge clk_25) quadBr <= {quadBr[1:0], quadB};
 
-always @(posedge ms_counter)
-if(quadA)
-begin
-    if(~&PaddlePosition)        // make sure the value doesn't overflow
-    PaddlePosition <= PaddlePosition + 1;
-end
-else if(quadB)
-begin
-   if(|PaddlePosition)        // make sure the value doesn't underflow
-   PaddlePosition <= PaddlePosition - 1;
+// paddle logic
+always @(posedge ms_counter) begin
+    if(quadA) begin
+        if(~&paddle_x_pos)          // make sure the value doesn't overflow
+        paddle_x_pos <= paddle_x_pos + 1;
+    end else if(quadB) begin
+       if(|paddle_x_pos)            // make sure the value doesn't underflow
+       paddle_x_pos <= paddle_x_pos - 1;
+    end
 end
 
-reg [4:0] speed = 1;
-reg [9:0] ball_x_pos = 5;
-reg [9:0] ball_y_pos = 5;
-reg negative_x = 0;
-reg negative_y = 0;
-
+// ball logic
 always@(posedge ms_clk) begin
-    if(ms_counter%10 == 0) begin
+    if(ms_counter%10 == 0) begin    // each 10 ms move the ball
         case ({negative_x,negative_y})
-                2'b00: begin
-                    ball_x_pos = ball_x_pos + speed;
-                    ball_y_pos = ball_y_pos + speed;
-                end
-                2'b01: begin
-                    ball_x_pos = ball_x_pos + speed;
-                    ball_y_pos = ball_y_pos - speed;
-                end
-                2'b10: begin
-                    ball_x_pos = ball_x_pos - speed;
-                    ball_y_pos = ball_y_pos + speed;
-                end
-                2'b11: begin
-                    ball_x_pos = ball_x_pos - speed;
-                    ball_y_pos = ball_y_pos - speed;
-                end
-            endcase
-        end
-        else begin
+            2'b00: begin
+                ball_x_pos = ball_x_pos + ball_speed;
+                ball_y_pos = ball_y_pos + ball_speed;
+            end
+            2'b01: begin
+                ball_x_pos = ball_x_pos + ball_speed;
+                ball_y_pos = ball_y_pos - ball_speed;
+            end
+            2'b10: begin
+                ball_x_pos = ball_x_pos - ball_speed;
+                ball_y_pos = ball_y_pos + ball_speed;
+            end
+            2'b11: begin
+                ball_x_pos = ball_x_pos - ball_speed;
+                ball_y_pos = ball_y_pos - ball_speed;
+            end
+        endcase
+    end else begin
+        // x position tracking
         if(ball_x_pos <= 8) begin
             negative_x <= 0;
         end else if(ball_x_pos >= 610) begin
             negative_x <= 1;
         end
+
         if(ball_y_pos <= 0) begin
             negative_y <= 0;
-        end else if(ball_y_pos >= 480 || ((ball_x_pos+30 > PaddlePosition+8) && (ball_x_pos+30 < PaddlePosition+120) && (ball_y_pos+30 > 432))) begin
+        end else if(((ball_x_pos+30 > paddle_x_pos+8) && (ball_x_pos+30 < paddle_x_pos+120) && (ball_y_pos+30 > 432))) begin
             negative_y <= 1;
+            // add a point              points = points + 1;
+            // make the ball faster     ball_speed = ball_speed + 1;
+        end else if (ball_y_pos >= 480) begin
+            // lose flag
+            // RESET GAME
         end
     end
 end
 
 wire border = (x[9:3]==0) || (x[9:3]==79) || (y[8:3]==0) || (y[8:3]==59);
-wire paddle = (x>=PaddlePosition+8) && (x<=PaddlePosition+120) && (y[8:4]==27);
+wire paddle = (x>=paddle_x_pos+8) && (x<=paddle_x_pos+120) && (y[8:4]==27);
 wire ball =   (x>ball_x_pos) && (x<ball_x_pos+30) && (y>ball_y_pos) && (y<ball_y_pos+30);
 
+// needs to be muxed with multi states
+// example: (this)
 assign R[3] = border | paddle | ball;
 assign G[3] = border | paddle | ball;
 assign B[3] = border | paddle | ball;
